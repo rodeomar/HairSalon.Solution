ca9a383fce9b3372e97f569b14e3ee52d6774641
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-07-30 15:56:37 +0530

    Updated the layout in Home Index view and _Layout. Also Added Stylist Controller

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using MySqlConnector;
+
+public class StylistController : Controller
+{
+    // Helper method to get the database connection
+    private MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(DBConfiguration.ConnectionString);
+    }
+
+}
