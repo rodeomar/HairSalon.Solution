c0a9489d0234e0a41c6bd64f5faf9a040cd36efb
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-07-30 16:03:05 +0530

    Added Index Method in Stylist Controller and Add index View in Stylist View

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -9,5 +9,30 @@ public class StylistController : Controller
     {
         return new MySqlConnection(DBConfiguration.ConnectionString);
     }
+    // Action to view a list of all stylists
+    public IActionResult Index()
+    {
+        List<Stylist> stylists = new List<Stylist>();
+        using (MySqlConnection conn = GetConnection())
+        {
+            conn.Open();
+            string sql = "SELECT * FROM Stylist";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Stylist stylist = new Stylist
+                    {
+                        StylistId = reader.GetInt32("StylistId"),
+                        Name = reader.GetString("Name"),
+                        Specialties = reader.GetString("Specialties")
+                    };
+                    stylists.Add(stylist);
+                }
+            }
+        }
+        return View(stylists);
+    }
 
 }
