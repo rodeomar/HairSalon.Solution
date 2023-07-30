28f54e02bf300071ae8169ac2488c5de14c93747
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-07-30 16:46:29 +0530

    Added Searching

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using MySqlConnector;
+using System.Collections.Generic;
 
 public class StylistController : Controller
 {
@@ -9,6 +10,7 @@ public class StylistController : Controller
     {
         return new MySqlConnection(DBConfiguration.ConnectionString);
     }
+
     // Action to view a list of all stylists
     public IActionResult Index()
     {
@@ -35,4 +37,43 @@ public class StylistController : Controller
         return View(stylists);
     }
 
+    // Action to search for stylists by name
+    public IActionResult Search(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            // If the search query is empty or null, redirect to the Index action
+            return RedirectToAction("Index");
+        }
+
+        List<Stylist> searchResults = GetStylistsByName(name);
+        return View("Index", searchResults);
+    }
+
+    // Helper method to get stylists by name from the database
+    private List<Stylist> GetStylistsByName(string name)
+    {
+        List<Stylist> stylists = new List<Stylist>();
+        using (MySqlConnection conn = GetConnection())
+        {
+            conn.Open();
+            string sql = "SELECT * FROM Stylist WHERE Name LIKE @Name";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Name", $"%{name}%");
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Stylist stylist = new Stylist
+                    {
+                        StylistId = reader.GetInt32("StylistId"),
+                        Name = reader.GetString("Name"),
+                        Specialties = reader.GetString("Specialties")
+                    };
+                    stylists.Add(stylist);
+                }
+            }
+        }
+        return stylists;
+    }
 }
